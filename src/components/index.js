930fccd0c27fc051b719ec1fbc1a3496b16abff4
Kori <kexin@korix.top>
2022-12-09 02:34:00 +0800

    website template

---
+++ src/components/index.js
@@ -0,0 +1,28 @@
+import NavBar from './NavBar';
+import Hero from './Hero';
+import Stats from './Stats';
+import Business from './Business';
+import Billing from './Billing';
+import CardDeal from './CardDeal';
+import Testimonials from './Testimonials';
+import CTA from './CTA';
+import Footer from './Footer';
+import Clients from './Clients'
+
+export {
+  NavBar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+}
+
+
+
+
+
